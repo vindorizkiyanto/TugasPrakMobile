58bb6cc10667a91df55acc12cf9751d30877f607
vindorizkiyanto <vindorizky96@gmail.com>
2020-10-15 12:05:01 +0700

    praktikum

---
+++ MyFlexibleFragment/app/src/main/java/com/example/myflexiblefragment/CategoryFragment.java
@@ -0,0 +1,42 @@
+package com.example.myflexiblefragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class CategoryFragment extends Fragment implements View.OnClickListener {
+    private Button btnDetailCategory;
+    public CategoryFragment() {
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_category, container, false);
+        btnDetailCategory = (Button)view.findViewById(R.id.btn_detail_category);
+        btnDetailCategory.setOnClickListener(this);
+        return view;
+    }
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_detail_category){
+            DetailCategoryFragment mDetailCategoryFragment = new DetailCategoryFragment();
+            Bundle mBundle = new Bundle();
+            mBundle.putString(DetailCategoryFragment.EXTRA_NAME, "Lifestyle");
+            String description = "Kategori ini akan berisi produk-produk life style";
+            mDetailCategoryFragment.setArguments(mBundle);
+            mDetailCategoryFragment.setDescription(description);
+            FragmentManager mFragmentManager = getFragmentManager();
+            FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
+            mFragmentTransaction.replace(R.id.frame_container, mDetailCategoryFragment, DetailCategoryFragment.class.getSimpleName());
+            mFragmentTransaction.addToBackStack(null);
+            mFragmentTransaction.commit();
+        }
+    }
+}
